641d0b273392c54ad161cad2fb210b104a215097
SoroushNilton <S.Soroush.Rz@gmail.com>
2024-07-20 16:05:57 -0600

    added bigram counter till 12:33

--- main.py
+++ main.py
@@ -3,6 +3,25 @@ words = open('names.txt', 'r').read().splitlines()
 # %%
 words[:10]
 # %%
-import torch
+len(words)
+# %%
+min(len(word) for word in words)
+# %%
+max(len(words) for word in words)
+# %%
+max(words, key=len)
+# %%
+b = {}
+for word in words:
+    chs = ['<S>'] + list(word) + ['<E>']
+    for ch1, ch2 in zip(chs, chs[1:]):
+        bigram = (ch1, ch2)
+        b[bigram] = b.get(bigram, 0) + 1
+        # print(ch1, ch2)
+# %%
+b
+# %%
+sorted(b.items(), key= lambda kv: kv[1], reverse=True)
+# sorted(b.items(), key= lambda kv: -kv[1]) # this and the one above are similar
 
 # %%
